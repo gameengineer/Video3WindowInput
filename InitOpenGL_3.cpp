5855ed4a3305d48fd4eb16ca32eaaae9f5fa85b9
U-GA\jonste <jonste@ASG-3WQSNW1.ga.com>
2017-09-27 07:57:53 -0700

    Initial commit

---
+++ InitOpenGL_3.cpp
@@ -0,0 +1,149 @@
+//-----------------------------------------------------------------------------
+// InitOpenGL_3.cpp by Steve Jones 
+// Copyright (c) 2015-2016 Game Institute. All Rights Reserved.
+//
+// - Adds OpenGL data in console window
+// - Adds framebuffer size callback to set glViewport
+//-----------------------------------------------------------------------------
+#include <iostream>
+#include <sstream>
+
+#define GLEW_STATIC
+#include "GL/glew.h"	// Important - this header must come before glfw3 header
+#include "GLFW/glfw3.h"
+
+// Global Variables
+const char* APP_TITLE = "Introduction to Modern OpenGL - Hello Window 3";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+
+// Function prototypes
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void glfw_onFramebufferSize(GLFWwindow* window, int width, int height);
+void showFPS(GLFWwindow* window);
+
+
+//-----------------------------------------------------------------------------
+// Main Application Entry Point
+//-----------------------------------------------------------------------------
+int main()
+{
+	// Intialize GLFW 
+	// GLFW is configured.  Must be called before calling any GLFW functions
+	if (!glfwInit())
+	{
+		// An error occured
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	// forward compatible with newer versions of OpenGL as they become available but not backward compatible (it will not run on devices that do not support OpenGL 3.3
+
+	// Create an OpenGL 3.3 core, forward compatible context window
+	GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	if (pWindow == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context the current one
+	glfwMakeContextCurrent(pWindow);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Set the required callback functions
+	glfwSetKeyCallback(pWindow, glfw_onKey);
+	glfwSetFramebufferSizeCallback(pWindow, glfw_onFramebufferSize);
+
+	// OpenGL version info 
+	const GLubyte* renderer = glGetString(GL_RENDERER);
+	const GLubyte* version = glGetString(GL_VERSION);
+	std::cout << "Renderer: " << renderer << std::endl;
+	std::cout << "OpenGL version supported: " << version << std::endl;
+	std::cout << "OpenGL Initialization Complete" << std::endl;
+
+	// Rendering loop
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		showFPS(pWindow);
+
+		// Poll for and process events
+		glfwPollEvents();
+
+		// Render the scene
+		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(pWindow);
+	}
+
+	// Clean up
+	glfwTerminate();
+
+	return 0;
+}
+
+//-----------------------------------------------------------------------------
+// Is called whenever a key is pressed/released via GLFW
+//-----------------------------------------------------------------------------
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+//-----------------------------------------------------------------------------
+// Is called when the window is resized
+//-----------------------------------------------------------------------------
+void glfw_onFramebufferSize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//-----------------------------------------------------------------------------
+// Code computes the average frames per second, and also the average time it takes
+// to render one frame.  These stats are appended to the window caption bar.
+//-----------------------------------------------------------------------------
+void showFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// Limit text updates to 4 times per second
+	if (elapsedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		// The C++ way of setting the window title
+		std::ostringstream outs;
+		outs.precision(3);	// decimal places
+		outs << std::fixed
+			<< APP_TITLE << "    "
+			<< "FPS: " << fps << "    "
+			<< "Frame Time: " << msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		// Reset for next average.
+		frameCount = 0;
+	}
+
+	frameCount++;
+}
